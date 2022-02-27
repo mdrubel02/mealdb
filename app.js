e13524f47adf42394fb973f949b7dd96609084e0
mdrubel02 <mdrubel072125@gmail.com>
2022-02-27 13:22:19 +0600

    adde

--- app.js
+++ app.js
@@ -16,9 +16,27 @@ document.getElementById('button-addon').addEventListener('click',()=>{
 
 
 const display=(meals)=>{
-    console.log(meals);
+    // console.log(meals);
     const main=document.getElementById('main');
-    meals.forEach(meal=>console.log(meal.strMeal))
+    meals.forEach(meal=>{
+        console.log(meal)
+        const div=document.createElement('div')
+        div.innerHTML=`
+        <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <h4 class=" text-muted">${meal.strCategory}  ${meal.strArea} </h4>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+       </div>
+        `;
+        main.appendChild(div);
+    })
+    
     // for(const meal of meal){
     //     console.log(meal);
 
